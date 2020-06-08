1db829e0f6487a141d6ec6ceb3d7f6bed5a3e065
zxl2609630641 <2609630641@qq.com>
2020-06-08 18:53:20 +0800

    Initial commit

---
+++ src/main/java/com/example/demogit/DemogitApplication.java
@@ -0,0 +1,16 @@
+package com.example.demogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 垃圾额
+ */
+@SpringBootApplication
+public class DemogitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemogitApplication.class, args);
+    }
+
+}
