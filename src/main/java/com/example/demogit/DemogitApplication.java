418bed9e6f8005e329727faa720a6433e1ce2f17
zxl2609630641 <2609630641@qq.com>
2020-06-08 19:06:05 +0800

    提交人：zxl

--- src/main/java/com/example/demogit/DemogitApplication.java
+++ src/main/java/com/example/demogit/DemogitApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * 垃圾额
+ * 垃圾额221511
  */
 @SpringBootApplication
 public class DemogitApplication {
